bbeaf4e5fb4d517e05c24185a3243aabd277c5b0
ssss <salah@salah.ly>
2017-09-05 13:23:34 -0700

    new template

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^salah/$', views.salah, name='salah')
+    url(r'^post/(?P<post_number>\d+)/$', views.post_detail, name='post_detail'),
+    
 ]
 
