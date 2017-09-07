af77c2b24ab30797a7283109c9cd2a2b830edabe
ssss <salah@salah.ly>
2017-09-07 16:32:18 -0700

    new page added as new post

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<post_number>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new , name = 'post_new') ,
+
     
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
 
@@ -18,3 +19,9 @@ def post_detail (request,post_number):
     post = get_object_or_404 (Post ,pk = post_number)
    
     return render(request, 'blog/post_detail.html', {'post':post})
+
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
